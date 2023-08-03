be8aec9d6be6f7cb91839880851bef56c3beb6a1
chengtungtung <hsintung.cheng@gmail.com>
2023-08-03 15:27:11 +0800

    2308031526

--- trainingtung/src/tw/leetcode/test/MergeTwoSortedListstest.java
+++ trainingtung/src/tw/leetcode/test/MergeTwoSortedListstest.java
@@ -3,7 +3,23 @@ package tw.leetcode.test;
 import java.util.Arrays;
 
 public class MergeTwoSortedListstest {
-
+	//
+	//
+	ListNode list1 = new ListNode(1);
+	ListNode list2 = new ListNode(2,list1);
+	ListNode list3 = new ListNode(4,list2);
+	
+	class ListNode {
+		 int val;
+		
+		 ListNode next;
+	
+		 ListNode() {}
+		 ListNode(int val) { this.val = val; }
+		 ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		 }
+	//		
+	//
 	public static void main(String[] args) {
 		int[] list1 = {1,2,4};
 		int[] list2 = {1,3,4};
