65b5c5177bf92b5421a04c3a322be6a7cc41cdbf
chengtungtung <hsintung.cheng@gmail.com>
2023-08-12 18:15:54 +0800

    0812

--- trainingtung/src/tw/leetcode/test/MergeTwoSortedListstest.java
+++ trainingtung/src/tw/leetcode/test/MergeTwoSortedListstest.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class MergeTwoSortedListstest {
 	Solution test = new Solution();
-	ListNode list12 = new ListNode(4);
-	ListNode list11 = new ListNode(2, list12);
+	ListNode list12 = new ListNode(5);
+	ListNode list11 = new ListNode(3, list12);
 	ListNode list1 = new ListNode(1, list11);
 
-	ListNode list22 = new ListNode(4);
-	ListNode list21 = new ListNode(3, list22);
-	ListNode list2 = new ListNode(1, list21);
+	ListNode list22 = new ListNode(6);
+	ListNode list21 = new ListNode(4, list22);
+	ListNode list2 = new ListNode(2, list21);
 
 	public static void main(String[] args) {
 		MergeTwoSortedListstest test1 = new MergeTwoSortedListstest();
@@ -21,32 +21,62 @@ public class MergeTwoSortedListstest {
 		System.out.println(test1.list1.next.next.next.val);
 		System.out.println(test1.list1.next.next.next.next.val);
 		System.out.println(test1.list1.next.next.next.next.next.val);
+	
 	}
-
 	class Solution {
-		
+
 		public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+			ListNode nextNode = new ListNode(); 
+			ListNode newNode = new ListNode(); 
+			ListNode nowNode1 = list1; 
+			ListNode nowNode2 = list2; 
 			
-			if(list1.val<=list2.val) {
-				list1.next = list2;
-				System.out.println("ok");
-			}else {
-				list2.next = list1;	
+			while(nowNode2 != null) {
+				if(nowNode1.val <= nowNode2.val) {
+					ListNode newone = new ListNode(nowNode2.val);
+					ListNode temp = nowNode1.next;
+					nowNode1.next = newone;
+					newone.next = temp; 
+					nowNode1 = newone.next;
+					nowNode2 = nowNode2.next;
+				}else {
+					
+				}
+				
 			}
 			
+			
+			
+			if (list1.val <= list2.val) {
+				nextNode = list1.next;
+				list1.next = list2;
+				list1.next.next.next.next= nextNode;
+				System.out.println("ok");
+			} else {
+				list2.next = list1;
+			}
+
 			return list1;
 		}
-
-
 	}
+
 	class ListNode {
 		int val;
 		ListNode next;
 
-		ListNode() {}
+		ListNode() {
+		}
 
-		ListNode(int val) {this.val = val;}
+		ListNode(int val) {
+			this.val = val;
+		}
 
-		ListNode(int val, ListNode next) {this.val = val;this.next = next;}
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
 	}
+
+	
+	
 }
